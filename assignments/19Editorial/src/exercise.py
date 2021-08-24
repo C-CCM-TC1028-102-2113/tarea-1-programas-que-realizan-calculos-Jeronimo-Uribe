029e22f088f1581e5fcdea512440cd1fb631b9db
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-24 11:15:41 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
+    npal=int(input('Dame el número de palabras: '))
+    npag=math.ceil(npal/475)
+    c1=npag*60
+    c2=c1-(c1*.1)
+    print('El costo de la publicación es: %f' %c2)
     pass
 
 
